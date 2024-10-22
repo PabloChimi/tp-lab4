9c0f6e3acbfecdea83aef94bd144f7e160c11614
Pablo <pchimiski@baufest.com>
2024-10-22 15:17:56 -0300

    Agrego lazy load

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,31 +1,50 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './component/home/home.component';
-import { AboutComponent } from './component/about/about.component';
-import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
-import { LoginComponent } from './component/login/login.component';
-import { RegisterComponent } from './component/register/register.component';
-import { AhorcadoComponent } from './component/juegos/ahorcado/ahorcado.component';
-import { MayormenorComponent } from './component/juegos/mayormenor/mayormenor.component';
-import { LaberintoComponent } from './component/juegos/laberinto/laberinto.component';
-import { PreguntadosComponent } from './component/juegos/preguntados/preguntados.component';
-import { ResultadosComponent } from './component/resultados/resultados.component';
-import { EncuestaComponent } from './component/encuesta/encuesta.component';
 
 export const routes: Routes = [
     // Si le ponemos 'prefix' nos va a arrojar un error en la consola de redireccion infinita
     { path: '', redirectTo: '', pathMatch: "full" },
-    { path: '', component: HomeComponent },
-    { path: 'about', component: AboutComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent},
-    { path: 'ahorcado', component: AhorcadoComponent },
-    { path: 'mayoromenor', component: MayormenorComponent },
-    { path: 'laberinto', component: LaberintoComponent },
-    { path: 'preguntados', component: PreguntadosComponent },
-    { path: 'resultados', component: ResultadosComponent },
-    { path: 'encuesta', component: EncuestaComponent },
-
-    // La ruta comodin debe ir siempre al final
-    { path: '**', component: PageNotFoundComponent },
-    
+    {
+        path: '',
+        loadComponent: () => import('./component/home/home.component').then(c => c.HomeComponent)
+    },
+    {
+        path: 'about',
+        loadComponent: () => import('./component/about/about.component').then(c => c.AboutComponent)
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./component/login/login.component').then(c => c.LoginComponent)
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('./component/register/register.component').then(c => c.RegisterComponent)
+    },
+    {
+        path: 'ahorcado',
+        loadComponent: () => import('./component/juegos/ahorcado/ahorcado.component').then(c => c.AhorcadoComponent)
+    },
+    {
+        path: 'mayoromenor',
+        loadComponent: () => import('./component/juegos/mayormenor/mayormenor.component').then(c => c.MayormenorComponent)
+    },
+    {
+        path: 'laberinto',
+        loadComponent: () => import('./component/juegos/laberinto/laberinto.component').then(c => c.LaberintoComponent)
+    },
+    {
+        path: 'preguntados',
+        loadComponent: () => import('./component/juegos/preguntados/preguntados.component').then(c => c.PreguntadosComponent)
+    },
+    {
+        path: 'resultados',
+        loadComponent: () => import('./component/resultados/resultados.component').then(c => c.ResultadosComponent)
+    },
+    {
+        path: 'encuesta',
+        loadComponent: () => import('./component/encuesta/encuesta.component').then(c => c.EncuestaComponent)
+    },
+    {
+        path: '**',
+        loadComponent: () => import('./component/page-not-found/page-not-found.component').then(c => c.PageNotFoundComponent)
+    }
 ];
